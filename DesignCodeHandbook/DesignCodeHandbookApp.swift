b8182bfa7f0cc085b69f085eb75d4bc840b007c1
Jianan Li <ljnlucy@gmail.com>
2022-12-18 00:41:02 -0500

    Initial Commit

---
+++ DesignCodeHandbook/DesignCodeHandbookApp.swift
@@ -0,0 +1,17 @@
+//
+//  DesignCodeHandbookApp.swift
+//  DesignCodeHandbook
+//
+//  Created by Jianan Li on 12/18/22.
+//
+
+import SwiftUI
+
+@main
+struct DesignCodeHandbookApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
